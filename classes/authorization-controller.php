8314b9740458eab3bbd6a0585653765e06060aa7
sohag07hasan <hyde.sohag@gmail.com>
2013-07-22 04:12:42 +0600

    working

--- classes/authorization-controller.php
+++ classes/authorization-controller.php
@@ -6,8 +6,43 @@ class AuthorizationController{
 	
 	static function init(){
 		add_action('init', array(get_class(), 'authorization_register_taxonomy'));
-		add_action('init', array(get_class(), 'api_check'), 100);
+		//add_action('init', array(get_class(), 'api_check'), 100);
+		
+		add_filter('the_content', array(get_class(), 'content_checking'));
+		add_filter('the_title', array(get_class(), 'title_checking'));
+		//add_action('init', array(get_class(), ''))
+		
+		
+		add_action('wp_enqueue_scripts', array(get_class(), 'enqueue_scripts'));
 	}
+	
+	
+	/**
+	 * Enqueue scripts (css/js)
+	 */
+	static function enqueue_scripts(){
+		
+		
+		/*
+		wp_enqueue_script('jquery');
+		wp_register_script('revealmodal_tenten_js', WP1010DATA_URL . 'assets/reveal/jquery.reveal.js', array('jquery'));
+		wp_enqueue_script('revealmodal_tenten_js');		
+		wp_register_style('revealmodal_tenten_css', WP1010DATA_URL . 'assets/reveal/reveal.css');
+		wp_enqueue_style('revealmodal_tenten_css');
+		
+		
+		wp_register_script('leanmodal_tenten_js', WP1010DATA_URL . 'assets/lean/jquery.leanModal.min.js', array('jquery'));
+		wp_enqueue_script('leanmodal_tenten_js');
+		
+		wp_register_script('tenten_driver_js', WP1010DATA_URL . 'js/tenten.js', array('jquery'));
+		wp_enqueue_script('tenten_driver_js');
+		*/
+		
+		wp_register_style('tenten_driver_css', WP1010DATA_URL . 'css/tenten.css');
+		wp_enqueue_style('tenten_driver_css');
+		
+	}
+	
 	
 	static function api_check(){
 		$tenten = new TenTenDataDotCom('mhasan', '1010data123');
@@ -18,8 +53,8 @@ class AuthorizationController{
 	}
 	
 	/**
-	 * register taxonomy
-	 * not hierarchy
+	 * register taxonomy (tag)
+	 * 
 	 * */
 	static function authorization_register_taxonomy(){
 		$labels = array(
@@ -52,6 +87,57 @@ class AuthorizationController{
 		);
 				
 		register_taxonomy(self::taxonomy, array_values(get_post_types()), $args );
+	}
+	
+	
+	/**
+	 * parsing links to get the tenten information
+	 * */
+	static function content_checking($content){
+		global $post;
+		/*		
+		$modal_markup = '<div id="myModal" class="reveal-modal">
+						     <h1>Modal Title</h1>
+						     <p>Any content could go in here.</p>
+						     <a class="close-reveal-modal">&#215;</a>
+						</div>';
+						
+		
+		//return $content . $modal_markup;
+		$lean_modal = '<div><a href="#myModal">Click Here</a></div>';
+		*/
+		
+		$tenten_groups = wp_get_object_terms($post->ID, self::taxonomy, array('fields' => 'names'));
+
+		if($tenten_groups){
+			
+		}
+		
+		return $content;
+		
+	}
+	
+	/**
+	 * return the user status
+	 * */
+	static function user_credentails_exists(){
+		
+	}
+	
+	
+	
+	//title chekcing
+	static function title_checking($title){
+		return $title;
+	}
+	
+	
+	/**
+	 * extract every anchor links from a string
+	 * */
+	static function grab_links($content){
+		if(preg_match_all('#href="(.*?)"#', $content, $match)) return $match[1];
+		return array();
 	}
 	
 }
---
+++ js/tenten.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($){
+	//alert('go');
+	//$('#myModal').reveal();
+	$("#modal-starter").leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" });
+});
